7b8841089d10d3b4e7f9563e62aa7066a37e7180
gugamaia <gustavojv_maia@outlook.com>
2023-10-19 14:28:03 -0300

    marcador vida

--- javaScript/marcador-de-vida/index.js
+++ javaScript/marcador-de-vida/index.js
@@ -1,4 +1,20 @@
-classe1
-classe2
-classe3
-classe4
+function marcadorVida (vida, classe) {
+    let novaVida = 0;
+
+    if (classe === 1) {
+        novaVida = vida -20;     
+    } else if (classe === 2) {
+        novaVida = vida - 30;
+    } else if (classe === 3) {
+        novaVida = vida -40;
+    } else {
+        novaVida = vida - 50;
+    }
+
+    if (novaVida <= 0) {
+        return "PERDEU";
+    } else {
+        return novaVida;
+    }
+}
+
